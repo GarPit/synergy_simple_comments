617bde390244084e7a534f93b1962c200f41fe5f
piton <piton@piton-desktop.(none)>
2012-08-02 12:38:54 +0400

    removed numericality validation

--- app/models/simple_comment.rb
+++ app/models/simple_comment.rb
@@ -1,7 +1,6 @@
 class SimpleComment < ActiveRecord::Base
   default_scope order("created_at DESC")
   attr_accessible :name, :email, :phone, :message
-  validates :name, :presence => true
-  validates :phone, :presence => true, :numericality => true
+  validates :name, :phone, :presence => true
   validates :email, :format => /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i, :allow_blank =>true
 end
